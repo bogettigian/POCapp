56844cc78528e5a6cf3571a07ee11670dae83227
santibozzo <santiagobozzo95@gmail.com>
2019-01-23 20:41:28 -0300

    boton andando

---
+++ Assets/texto.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class texto : MonoBehaviour {
+
+	private int veces;
+	public Text textoAMostrar;
+
+	// Use this for initialization
+	void Start () {
+		veces = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void sumarPresion() {
+		veces++;
+		textoAMostrar.text = "Veces presionado: " + veces;
+	}
+}
