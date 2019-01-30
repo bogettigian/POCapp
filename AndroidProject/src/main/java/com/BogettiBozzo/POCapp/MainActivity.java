f7b33d5082499cc632cdac46f128709aed681a8e
Gianfranco Bogetti <gianbogetti7@hotmail.com>
2019-01-30 18:08:44 -0300

    Siempre tengo que arreglar todo yo

--- AndroidProject/src/main/java/com/BogettiBozzo/POCapp/MainActivity.java
+++ AndroidProject/src/main/java/com/BogettiBozzo/POCapp/MainActivity.java
@@ -1,6 +1,6 @@
 package com.BogettiBozzo.POCapp;
 
-import android.support.v7.appd.AppCompatActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
