311f35f5e0b1b86c2645364fea469b364f7cefc3
santibozzo <satiagobozzo95@gmail.com>
2019-01-30 20:16:09 -0300

    POC finalizada

--- AndroidProject/src/main/java/com/BogettiBozzo/POCapp/MainActivity.java
+++ AndroidProject/src/main/java/com/BogettiBozzo/POCapp/MainActivity.java
@@ -1,13 +1,21 @@
 package com.BogettiBozzo.POCapp;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void runUnity(View view){
+        System.out.println("running Unity");
+        startActivity(new Intent(MainActivity.this, UnityPlayerActivity.class));
     }
 }
